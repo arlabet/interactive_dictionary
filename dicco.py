9a36825d3bfdc3045ad27550660686c2965d54a6
arlabet <sahloumi.najima@gmail.com>
2020-07-06 17:13:29 +0200

    improvement of cases

--- dicco.py
+++ dicco.py
@@ -8,11 +8,13 @@ def definit(word):
 	if word in data:
 		return ("\n- ".join(data[word]))
 	elif len(get_close_matches(word, data.keys())) > 0:
-		yn = input("Did you mean %s instead ? Enter Y if yes or N if no " % get_close_matches(word, data.keys())[0])
+		yn = input("Did you mean %s instead ? Enter Y if yes or N if no: " % get_close_matches(word, data.keys())[0])
 		if yn == "Y":
 			return ("\n- ".join(data[get_close_matches(word, data.keys())[0]]))
-		if yn == "N":
+		elif yn == "N":
 			return ("This word doesn't exist")
+		else:
+			return ("I dodn't understand what you mean")
 	else:
 		return ("This word doesn't exist")
 
