53bb00d1a237d476b55feb25996377702aa6d20d
arlabet <sahloumi.najima@gmail.com>
2020-07-06 17:32:58 +0200

    nom propre

--- dicco.py
+++ dicco.py
@@ -4,7 +4,8 @@ from difflib import get_close_matches
 data = json.load(open("data.json"))
 
 def definit(word):
-	word = word.lower()
+	if (word in data) == False:
+		word = word.lower()
 	if word in data:
 		return (data[word])
 	elif len(get_close_matches(word, data.keys())) > 0:
