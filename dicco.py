066a11cf652637ca90962685f676a1f33ef7ea4a
arlabet <sahloumi.najima@gmail.com>
2020-07-06 17:36:48 +0200

    uppercases

--- dicco.py
+++ dicco.py
@@ -4,10 +4,13 @@ from difflib import get_close_matches
 data = json.load(open("data.json"))
 
 def definit(word):
-	if (word in data) == False:
-		word = word.lower()
+	word = word.lower()
 	if word in data:
 		return (data[word])
+	elif word.title() in data:
+		return data[word.title()]
+	elif word.upper() in data:
+		return data[word.upper()]
 	elif len(get_close_matches(word, data.keys())) > 0:
 		yn = input("Did you mean %s instead ? Enter Y if yes or N if no: " % get_close_matches(word, data.keys())[0])
 		if yn == "Y":
